a40d6944ab42edb87d26721d510749a21371e7de
Andrew Miller <amiller@gwggroup.com>
2023-07-23 11:48:52 -0700

    Update 1 for Azure challenges completed thus far

--- src/model/train.py
+++ src/model/train.py
@@ -3,16 +3,17 @@
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -22,7 +23,6 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -34,6 +34,11 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X, y = df.loc[:,'PatientID':'Age'], df.loc[:,"Diabetic"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,\
+                                                 random_state=42, shuffle=True)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
