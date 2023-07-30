1e5822f323af2aa2673036cc607ec2a30933e315
Andrew Miller <amiller@gwggroup.com>
2023-07-30 12:05:37 -0700

    try again

--- src/model/train.py
+++ src/model/train.py
@@ -38,7 +38,7 @@ def get_csvs_df(path):
 def split_data(df):
     X, y = df.loc[:, 'PatientID':'Age'], df.loc[:, "Diabetic"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,
-                                                            random_state=42, shuffle=True)
+                                                        random_state=42, shuffle=True)
     return X_train, X_test, y_train, y_test
 
 
@@ -63,6 +63,7 @@ def parse_args():
     # return args
     return args
 
+
 # run script
 if __name__ == "__main__":
     # add space in logs
@@ -74,7 +75,6 @@ if __name__ == "__main__":
 
     # run main function
     main(args)
-
 
     # add space in logs
     print("*" * 60)
